5d1be5bec0434b105d23cfcc0258f43ec9c46fce
wujide <wujide@zillionfortune.com>
2017-06-21 18:04:08 +0800

    add testcase

--- demo/decoration_test.py
+++ demo/decoration_test.py
@@ -4,10 +4,16 @@
 import time
 
 
+def log(func):
+    def wrapper(*args, **kwargs):
+        print "call %s():" % func.__name__
+        return func(*args, **kwargs)
+    return wrapper()
+
+
+@log
 def now():
     print "now:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-
-
-now()
+now
 
