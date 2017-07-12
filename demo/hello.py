f6356620270ae40ebc35a96be833148ab139cd23
wujide <wujide@zillionfortune.com>
2017-07-12 09:39:37 +0800

    add flask case

--- demo/hello.py
+++ demo/hello.py
@@ -4,9 +4,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!!'
+
+
+@app.route('/post/<int:num>')
+def hello_num(num):
+    return 'Hello %d!' % num
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
