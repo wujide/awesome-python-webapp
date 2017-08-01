089ec6df84016f8c731dc884663637c403fa4795
wujide <wujide@zillionfortune.com>
2017-08-01 21:53:08 +0800

    modify cases

--- demo/mock_test.py
+++ demo/mock_test.py
@@ -4,21 +4,30 @@ import urllib
 
 import mock
 import unittest
-import import_test
-from import_test import visit_ustack
+import client
+from client import visit_ustack
 
 
 class TestClient(unittest.TestCase):
 
     def test_success_request(self):
         success_send = mock.Mock(return_value='200')
-        import_test.send_request = success_send
+        client.send_request = success_send
         self.assertEqual(visit_ustack(), '200')
 
     def test_fail_request(self):
         fail_send = mock.Mock(return_value='404')
-        import_test.send_request = fail_send
+        client.send_request = fail_send
         self.assertEqual(visit_ustack(), '404')
+
+    def test_call_send_request_with_right_arguments(self):
+        client.send_request = mock.Mock()
+        client.visit_ustack()
+        if self.assertEqual(client.send_request.called, True):
+            print "called"
+        call_args = client.send_request.call_args
+        self.assertIsInstance(call_args[0][0], str)
+        print call_args[0][0]
 
     def runTest(self):
         pass
@@ -27,3 +36,4 @@ if __name__ == "__main__":
     tc = TestClient()
     tc.test_success_request()
     tc.test_fail_request()
+    tc.test_call_send_request_with_right_arguments()
