c36405a3e071e400ca00a411b8a445967273ea66
wujide <wujide2015@126.com>
2017-08-07 22:15:59 +0800

    add json cases

---
+++ flask_demo/json_demo.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+# __author__='Administrator'
+
+from flask import Flask, jsonify
+import json
+
+app = Flask(__name__)
+data = {"name": "Tacey", "age": 13, "sex": "male", "interst": ("Programing", "Reading")}
+
+# json.dumps
+json_data = json.dumps(data)
+print json_data
+
+# json.loads
+name = json.loads(json_data)['name']
+print name
+
+
+@app.route('/jsonify', methods=['get'])
+def get_json():
+    return jsonify({'data': json_data})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
