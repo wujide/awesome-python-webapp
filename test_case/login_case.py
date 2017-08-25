2bf65d6eb25581c9b9a130a81c439c8d12684eb7
wujide <wujide@zillionfortune.com>
2017-08-25 17:41:31 +0800

    modify cases

--- test_case/login_case.py
+++ test_case/login_case.py
@@ -5,13 +5,13 @@ import urllib
 import urllib2
 import cPickle
 from flask import json, jsonify
-
+'''
 with open(r'../info/user_pwd', 'r') as f:
     lines_strip = f.readline().split()
     phoneNum = lines_strip[0].split('=')[1]
     loginpwd = lines_strip[1].split('=')[1]
     print "phoneNum, loginpwd:", phoneNum, loginpwd
-
+'''
 try:
     import cPickle as pickle
 except ImportError:
@@ -21,27 +21,28 @@ except ImportError:
 def data_get():
     with open(r"../info/user_pwd_dict.txt", 'rb') as f:
         d = f.read()
-        data = json.loads(json.dumps(d))
-        print type(data)
-        print data
-        return data
+        data = json.dumps(d)
+        # print "type(data):", type(data)
+        values = eval(json.loads(data))
+        # print values['user']
+        print "type(values):", type(values)
+        return values
 
 
-
-# post
 def login():
-    url = 'https://app.91yaowang.com/app/webservice/v2/member/login'
-    values = {"phoneNum": phoneNum, "loginpwd": loginpwd}
+    values = data_get()
+    # print values
     data = urllib.urlencode(values)
-    req = urllib2.Request(url)
+    req = urllib2.Request(values['url'])
     response = urllib2.urlopen(req, data)
     print response.read()
     # 序列化并保存到文件中
-    date_return = json.load(response)
+    dt = urllib.urlencode(response)
+    date_return = json.load(dt)
     write_to_file(date_return)
     # print date_return
     # print type(date_return)
-    loginToken = date_return['data']['loginToken']
+    # loginToken = date_return['data']['loginToken']
     # print "loginToken = ", loginToken
     # d = dict(loginToken=loginToken)
 
@@ -55,3 +56,4 @@ if __name__ == "__main__":
     login()
     # login_get()
     # data_get()
+
