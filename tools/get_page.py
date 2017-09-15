6ea19fd1e73b29172a0c8ef1fc90729673f8f6b9
wujide <wujide@zillionfortune.com>
2017-09-15 16:49:10 +0800

    optimize cases

--- tools/get_page.py
+++ tools/get_page.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 # __author__='wujide'
+import re
 import urllib2
 
 
@@ -10,9 +11,25 @@ class GetPage:
     def getpage(self):
         request = urllib2.Request(self.total_url)
         response = urllib2.urlopen(request)
-        print response.read().decode('gbk')
+        # print response.read().decode('gbk')
         return response.read().decode('gbk')
+
 
 url = 'http://mm.taobao.com/json/request_top_list.htm'
 gt = GetPage(url, 'page', 1)
-gt.getpage()
+page = gt.getpage()
+
+
+def getContents():
+
+    pattern = re.compile(
+        '<div class="list-item".*?pic-word.*?<a href="(.*?)".*?<img src="(.*?)".*?<a class="lady-name.*?>(.*?)</a>.*?<strong>(.*?)</strong>.*?<span>(.*?)</span>',
+        re.S)
+    items = re.findall(pattern, page)
+    contents = []
+    for item in items:
+        contents.append([item[0], item[1], item[2], item[3], item[4]])
+    print contents
+    return contents
+
+getContents()
