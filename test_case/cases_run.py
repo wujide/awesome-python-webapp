2c31133e8e84796d97c711be82189f0d2c95dde6
wujide <wujide@zillionfortune.com>
2017-09-23 16:46:50 +0800

    add case

--- test_case/cases_run.py
+++ test_case/cases_run.py
@@ -7,7 +7,8 @@ from buy_product_case import buyProduct
 from pig_amount_to_base_amount import pigAmountToBaseAmount
 from hytx_personal import personal
 from hytx_level import level
-
+from test_case import lcds_investcount
+from test_case.lcds_simple_info import lcds_simpleinfo
 
 login()
 # 0 元白拿获取投资值和邀请值
@@ -20,4 +21,8 @@ pigAmountToBaseAmount()
 personal()
 # 会员体系 - 用户等级
 level()
+# 理财大使：基础信息
+lcds_simpleinfo
+# 理财大使：好友在投，一级好友人数
+lcds_investcount
 
