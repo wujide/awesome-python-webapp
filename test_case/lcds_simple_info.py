363ce14ad463290d1aecb0cb2dd4f6c321c0997c
wujide <wujide@zillionfortune.com>
2017-09-23 16:31:39 +0800

    add case

---
+++ test_case/lcds_simple_info.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+# __author__='wujide'
+
+
+from interface_test_class import InterfaceTest
+
+
+def lcds_simpleinfo():
+    para_path = r"../info/lcds_simpleinfo_case_para.txt"
+    getInvestInfo_obj = InterfaceTest(para_path)
+    getInvestInfo_obj.para_get(para_path=para_path, iterface_url='url_lcds_simpleinfo')
+    values = getInvestInfo_obj.data_get()
+    response = getInvestInfo_obj.data_post(values)
+    data = response.read()  # <type 'str'>
+    # print data
+    file_save = r"../data/lcds_simpleinfo"
+    getInvestInfo_obj.data_save(file_save, data)
+    getInvestInfo_obj.pass_or_fail(file_save)
+    return data
+
+if __name__ == '__main__':
+    print lcds_simpleinfo()
